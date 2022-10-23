9c0d1460e2e895aae747742ca25e55930dfdec8d
Pranesh Pyara Shrestha <80845871+praneshp1org@users.noreply.github.com>
2022-10-23 11:13:48 +0545

    added simple dumb html

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express'),
     app = express(),
     cors = require('cors'),
     mysql = require('mysql'),
+    path = require('path'),
     bodyParser = require('body-parser');
 const usersRouter = require('./routes/users');
 
@@ -17,11 +18,18 @@ const server = {
     port: 8080
 };
 
+
+
 // use the modules
 app.use(cors())
 app.use(bodyParser.json());
-//middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+    //middleware
 app.use('/users', usersRouter);
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname) + '/input.html');
+});
 
 // starting the server
 app.listen(server.port, () => console.log(`Server started, listening port: ${server.port}`));
